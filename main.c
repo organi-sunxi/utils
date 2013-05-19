14b4c79322d97ef34b4d31bde032692b1bccebff
zhoumm0510 <zhoumm0510@d1321ab1-fb09-4814-9f2d-e005d23f7cc0>
2013-05-19 08:11:08 +0000

    add a function for settting configure file into envirment.
    git-svn-id: https://localhost/svn/svnTest/trunk/emconfig@72 d1321ab1-fb09-4814-9f2d-e005d23f7cc0

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "logmessage.h"
 
 #define MAX_COMMAND 256
+#define MAX_STRING 256
 #define DEFAULT_CONFIG_FILE	"/data/etc/emconfig.conf"
 
 
@@ -21,6 +22,51 @@ static void print_usage (const char*program_name, int exit_code)
 		);
 	exit (exit_code); 
 }
+
+static void flit_string(const char *src, char *value_name, char *value_path)
+{
+	int count;
+	const char *tmp = NULL;
+	
+	if (!src) {
+		value_name = NULL;
+		value_path = NULL;
+		return;
+	}
+
+	count = 0;
+	tmp = src;
+	while ((*tmp != '=') && (*tmp!= '\0')) {
+		tmp++;
+		count++;
+	}
+	strncpy(value_name, src, count);
+	
+	src += ++count;
+	strncpy(value_path, src, strlen(src) - 1);
+}
+
+static void put_env()
+{
+	char path[MAX_STRING];
+	char value_name[MAX_STRING];
+	char value_path[MAX_STRING];
+	int path_len;
+	FILE *config_file = fopen(DEFAULT_CONFIG_FILE, "r");
+
+	if (!config_file) {
+		return;
+	}
+	
+	while (fgets(path, sizeof(path), config_file)) {
+		flit_string(path, value_name, value_path);
+		setenv(value_name, value_path, 1);
+	}
+	
+	fclose(config_file);
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -54,6 +100,8 @@ int main(int argc, char *argv[])
 	}while (next_option !=-1); 
 
 	//to do: set configure file into envirment
+
+	put_env();
 
 	for (;;) {
 		printf(">");
