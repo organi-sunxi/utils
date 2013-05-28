fe00f61b3ee1f4563835e1dda572a0e289c3cd9c
threewater <threewaterL@163.com>
2013-05-28 20:19:58 +0800

    need't flip bitmap

--- mksplash/mksplash.c
+++ mksplash/mksplash.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <getopt.h>
 
 #define DEFAULT_LOAD_ADDR	0x43100000
@@ -37,26 +38,147 @@ typedef struct {
 	uint16_t cx, cy;
 }splash_header;
 
-static void write_file16(FILE *pfin, FILE *pfout)
+static int bitmap24to16(unsigned char *data, unsigned char *out)
 {
-	unsigned char data[4];
-	unsigned short od;
+	uint16_t *p = (uint16_t *)out;
 
-	while(fread(data, 3, 1, pfin)){
-		od=((data[2]&0xf8)<<8) | ((data[1]&0xfc)<<3) |(data[0]>>3);
-		fwrite(&od, 2, 1, pfout);
-	}
+	*p=((data[2]&0xf8)<<8) | ((data[1]&0xfc)<<3) |(data[0]>>3);
+	return 2;
 }
 
-static void write_file32(FILE *pfin, FILE *pfout)
+static int bitmap24to32(unsigned char *data, unsigned char *out)
 {
-	unsigned char data[4];
-	uint32_t od;
+	memcpy(out, data, 4);
+	return 4;
+}
 
-	while(fread(data, 3, 1, pfin)){
-		memcpy(&od, data, sizeof(data));
-		fwrite(&od, sizeof(od), 1, pfout);
+struct output_fmt{
+	int bit;
+	int	uimage;
+	unsigned long loadaddr;
+	int x, y;
+	int width,height;
+	char *infilename;
+	char *outfilename;
+	char *tmpfilename;
+	int (*bitmap24to)(unsigned char *, unsigned char *);
+};
+
+static int bmp2splash(struct output_fmt *fmt)
+{
+	FILE *pfin, *pfout;
+	char buf[400], data[4], od[4];
+	int cx, cy, byteperline, w, h;
+	BITMAPFILEHEADER bmpfileheader;
+	BITMAPINFOHEADER bmpinfoheader;
+	splash_header splashhd={SPLASH_MAGIC,};
+	int ret;
+
+	splashhd.x = fmt->x;
+	splashhd.y = fmt->y;
+
+	pfin = fopen(fmt->infilename, "r");
+
+	if(pfin==NULL){
+		printf("file %s open failed\n", fmt->infilename);
+		return -1;
 	}
+
+	fread(buf, 2, 1, pfin);
+
+	if(buf[0]!='B' ||buf[1] !='M'){
+		printf("Input file must be bitmap format!\n");
+		return -1;
+	}
+
+	fread(&bmpfileheader, sizeof(BITMAPFILEHEADER), 1, pfin);
+	fread(&bmpinfoheader, sizeof(BITMAPINFOHEADER), 1, pfin);
+
+	cx=bmpinfoheader.biWidth;
+	cy=bmpinfoheader.biHeight;
+	byteperline = cx * bmpinfoheader.biBitCount/8;
+
+	printf("width: %d, height: %d, %d bit\n", cx, cy, bmpinfoheader.biBitCount);
+
+	if(bmpinfoheader.biBitCount != 24){
+		printf("Bitmap file must be 24bit\n");
+		return -1;
+	}
+
+	pfout = fopen(fmt->outfilename, "w");
+	if(pfout==NULL){
+		printf("Create file %s failed\n", fmt->outfilename);
+		return -1;
+	}
+
+	splashhd.cx = cx;
+	splashhd.cy = cy;
+	
+	fwrite(&splashhd, sizeof(splashhd), 1, pfout);
+	fclose(pfout);
+
+	printf("write head ok! Create tmpfile\n");
+
+	pfout = fopen(fmt->tmpfilename, "wb");
+
+	if(pfout==NULL){
+		printf("Create file %s failed\n", fmt->tmpfilename);
+		return -1;
+	}
+
+	ret = fseek(pfin, (cy-1)*byteperline, SEEK_CUR);
+
+	for(h=0;h<cy;h++){
+		for(w=0;w<cx;w++){
+			ret = fread(data, 3, 1, pfin);
+			if(ret <= 0){
+				printf("failed to read\n");
+				goto failed1;
+			}
+			ret = fmt->bitmap24to(data, od);
+			fwrite(od, ret, 1, pfout);
+		}
+		fseek(pfin, -2*byteperline, SEEK_CUR);
+	}
+
+failed1:
+	fclose(pfout);
+	fclose(pfin);
+
+/////////////////////////////////////////////////
+	printf("write tmpfile ok! Create gzip...\n");
+
+	snprintf(buf, sizeof(buf), "gzip -f %s", fmt->tmpfilename);
+	ret = system(buf);
+
+	if(ret){
+		printf("compress error\n");
+		return 1;
+	}
+	else
+		printf("done\n");
+
+	pfout=fopen(fmt->outfilename, "a+");
+	snprintf(buf, sizeof(buf), "%s.gz", fmt->tmpfilename);
+	pfin = fopen(buf, "rb");
+
+	while(ret=fread(buf, 1, sizeof(buf),  pfin)){
+		fwrite(buf, 1, ret, pfout);
+	}
+
+	fclose(pfout);
+	fclose(pfin);
+	
+	snprintf(buf, sizeof(buf), "%s.gz", fmt->tmpfilename);
+	unlink(buf);
+
+	if(fmt->uimage){
+		rename(fmt->outfilename, fmt->tmpfilename);
+		snprintf(buf, sizeof(buf), "mkimage -A arm -C none -a 0x%lx -n splash -d %s u%s", fmt->loadaddr, fmt->tmpfilename, fmt->outfilename);
+		system(buf);
+		unlink(fmt->tmpfilename);
+	}
+	return 0;	
 }
 
 static void show_help(char *name)
@@ -71,148 +193,70 @@ static void show_help(char *name)
 
 int main(int argc, char* argv[])
 {
-	int i, bit=16, uimage;
-	unsigned long loadaddr=DEFAULT_LOAD_ADDR;
-	void (*write_file)(FILE*, FILE*);
-	FILE *pfin, *pfout;
-	char bmp[400];
-	int cx, cy;
-	BITMAPFILEHEADER bmpfileheader;
-	BITMAPINFOHEADER bmpinfoheader;
-	char *outfilename = OUTFILE_NAME;
-	splash_header splashhd={SPLASH_MAGIC,};
-	int ret;
+	int i;
+	struct output_fmt fmt={
+		.bit=16,
+		.uimage=0,
+		.loadaddr=DEFAULT_LOAD_ADDR,
+		.outfilename = OUTFILE_NAME,
+		.tmpfilename = TMPFILE_NAME,
+		.x=0,
+		.y=0,
+		.bitmap24to=NULL,
+	};
 
 	if(argc < 2){
 		show_help(argv[0]);
 		return -1;
 	}
 
-	while((i = getopt(argc, argv, "hx:y:b:o:a:u")) != -1)
-    {
+	while((i = getopt(argc, argv, "hx:y:b:o:a:ut:")) != -1){
         switch(i)
         {
             case 'h':
 				show_help(argv[0]);
 				return -1;
             case 'x':
-				splashhd.x = strtol(optarg, NULL, 0);
+				fmt.x = strtol(optarg, NULL, 0);
                 break;
             case 'y':
-				splashhd.y = strtol(optarg, NULL, 0);
+				fmt.y = strtol(optarg, NULL, 0);
                 break;
             case 'b':
-				bit = strtol(optarg, NULL, 0);
-				if(bit==32)
-					write_file = write_file32;
-				else if(bit=16)
-					write_file = write_file16;
+				fmt.bit = strtol(optarg, NULL, 0);
+				if(fmt.bit==32)
+					fmt.bitmap24to = bitmap24to32;
+				else if(fmt.bit=16)
+					fmt.bitmap24to = bitmap24to16;
                 break;
             case 'o':
-				outfilename = optarg;
+				fmt.outfilename = optarg;
+                break;
+            case 't':
+				fmt.tmpfilename = optarg;
                 break;
 			case 'u':
-				uimage = 1;
+				fmt.uimage = 1;
 				break;
             case 'a':
-				loadaddr = strtol(optarg, NULL, 0);
+				fmt.loadaddr = strtol(optarg, NULL, 0);
 				break;
         }
     }
 
-	if(!write_file){
-		printf("LCD bit only don't support %d\n", bit);
+	if(!fmt.bitmap24to){
+		printf("LCD bit only don't support %d\n", fmt.bit);
 		show_help(argv[0]);
 		return -1;
 	}
 
-	pfin = fopen(argv[optind], "r");
-
-	if(pfin==NULL){
-		printf("file %s open failed\n", argv[optind]);
+	if(argc < optind+1){
+		printf("no input file\n");
+		show_help(argv[0]);
 		return -1;
 	}
 
-	fread(bmp, 2, 1, pfin);
-
-	if(bmp[0]!='B' ||bmp[1] !='M'){
-		printf("Input file must be bitmap format!\n");
-		return -1;
-	}
-
-	fread(&bmpfileheader, sizeof(BITMAPFILEHEADER), 1, pfin);
-	fread(&bmpinfoheader, sizeof(BITMAPINFOHEADER), 1, pfin);
-
-	cx=bmpinfoheader.biWidth;
-	cy=bmpinfoheader.biHeight;
-
-	printf("width: %d, height: %d, %d bit\n", cx, cy, bmpinfoheader.biBitCount);
-
-	if(bmpinfoheader.biBitCount != 24){
-		printf("Bitmap file must be 24bit\n");
-		return -1;
-	}
-
-	pfout = fopen(outfilename, "w");
-
-	if(pfout==NULL){
-		printf("Create file %s failed\n", outfilename);
-		return -1;
-	}
-
-	splashhd.cx = cx;
-	splashhd.cy = cy;
-	
-	fwrite(&splashhd, sizeof(splashhd), 1, pfout);
-	fclose(pfout);
-
-	printf("write head ok! Create tmpfile\n");
-
-	pfout = fopen(TMPFILE_NAME, "wb");
-
-	if(pfout==NULL){
-		printf("Create file %s failed\n", outfilename);
-		return -1;
-	}
-
-	write_file(pfin, pfout);
-
-	fclose(pfout);
-	fclose(pfin);
-
-	//return 0;
-	//system ("ls -l *");
-
-/////////////////////////////////////////////////
-	printf("write tmpfile ok! Create gzip...\n");
-
-	ret = system("gzip "TMPFILE_NAME);
-
-	if(ret){
-		printf("compress error\n");
-		return 1;
-	}
-	else
-		printf("done\n");
-
-	pfout=fopen(outfilename, "a+");
-	pfin = fopen(TMPFILE_NAME".gz", "rb");
-
-	while(i=fread(bmp, 1, sizeof(bmp),  pfin)){
-		fwrite(bmp, 1, i, pfout);
-	}
-
-	fclose(pfout);
-	fclose(pfin);
-	
-	unlink(TMPFILE_NAME".gz");
-	rename(outfilename, TMPFILE_NAME);
-
-	if(uimage){
-		sprintf(bmp, "mkimage -A arm -C none -a 0x%lx -n splash -d "TMPFILE_NAME" u%s", loadaddr, outfilename);
-		system(bmp);
-		unlink(TMPFILE_NAME);
-	}
-	return 0;	
+	fmt.infilename = argv[optind];
+	bmp2splash(&fmt);
 }
 
